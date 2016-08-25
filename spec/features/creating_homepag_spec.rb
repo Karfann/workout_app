6fe79097161e862edabbe0d3ac63f176b9fd3d2b
= <karfannacif@gmail.com>
2016-08-24 22:47:13 -0300

    Add home page

--- spec/features/creating_homepag_spec.rb
+++ spec/features/creating_homepag_spec.rb
@@ -7,9 +7,7 @@ RSpec.feature "Creating Home Page" do
     visit '/'
 
     expect(page).to have_link('Athletes Den')
-
     expect(page).to have_link('Home')
-
     expect(page).to have_content('Workout Lounge')
 
   end
