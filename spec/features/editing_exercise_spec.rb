2a5d8870c30a272f3383cb89f0eb82c65ad2aadd
= <karfannacif@gmail.com>
2016-09-03 15:46:20 -0300

    Adjust spec after including first and last name

--- spec/features/editing_exercise_spec.rb
+++ spec/features/editing_exercise_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.feature "Editing Exercises" do 
 
   before do
-    @owner = User.create(email: "owner@example.com", password: "password")
-
+    @owner = User.create(first_name: "John", last_name: "Doe",email: "owner@example.com", password: "password")
+    
     @owner_exer = @owner.exercises.create!( duration_in_min: 45, 
                                             workout: "Cardio Activity",
                                             workout_date: Date.today )
